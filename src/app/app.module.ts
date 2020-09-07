26f099f5337771ba3ca70826b838035e81c8b264
Paco-Medina <pacomedina.05.12.99@outlook.com>
2020-09-06 19:25:27 -0500

    Componentes del navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactoComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
