eb1ff71fa59ff8a790d793ef80144e0adb56aa48
Paco-Medina <pacomedina.05.12.99@outlook.com>
2020-09-07 17:37:34 -0500

    Funcionan rutas, pero mamo el navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
+import { ProyectosComponent } from '../app/proyectos/proyectos.component';
+import { ContactoComponent } from '../app/contacto/contacto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'proyecto', component: ProyectosComponent },
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
