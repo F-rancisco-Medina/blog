93b781c8f7bd364c3660beb9026a4e00a2118542
Paco-Medina <pacomedina.05.12.99@outlook.com>
2020-10-15 11:53:23 -0500

    Primera parte del servicio

--- src/app/servicios/speak.service.ts
+++ src/app/servicios/speak.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import Speech from 'speak-tts';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpeakService {
 
-  constructor() { }
+  constructor() { 
+    console.log("Hola");
+  }
 }
