b793f770a25a73eb3836105972026a9188d3b38f
Paco-Medina <pacomedina.05.12.99@outlook.com>
2020-10-16 11:38:58 -0500

    Servicio en 3 componentes principales

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpeakService } from '../servicios/speak.service';
 
 @Component({
   selector: 'app-contacto',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private speak:SpeakService) { }
 
   ngOnInit(): void {
   }
--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpeakService } from '../servicios/speak.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private speak:SpeakService) { }
 
   ngOnInit(): void {
   }
