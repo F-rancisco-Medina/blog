07655b1eeb69c7674c3bb7e1082bc558041c421a
Paco-Medina <pacomedina.05.12.99@outlook.com>
2020-10-18 20:24:31 -0500

    Se inicializo el Spech y se declaro en el servicio

--- src/app/servicios/speak.service.ts
+++ src/app/servicios/speak.service.ts
@@ -7,8 +7,25 @@ import Speech from 'speak-tts';
   providedIn: 'root'
 })
 export class SpeakService {
+  speech:any;
+
 
   constructor() { 
     console.log("Hola");
+    this.speech = new Speech();
+    console.log("Se declaro");
+    if (this.speech.hasBrowserSupport()) {
+      // returns a boolean
+      console.log('speech synthesis supported');
+      this.speech.init({
+          volume: 1,
+          lang: 'es-MX',
+          rate: 1,
+          pitch: 1,
+          voice: 'Microsoft Sabina Desktop - Spanish (Mexico)',
+          splitSentences: true
+        });
+    }
+    console.log("SE configuro");
   }
 }
