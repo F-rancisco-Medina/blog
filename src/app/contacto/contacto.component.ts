967ddfeb0bd243ad38e85212428af9fe9e7308d5
Paco-Medina <pacomedina.05.12.99@outlook.com>
2020-10-22 10:52:45 -0500

    Funciones del servicio en los demas componentes

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -8,9 +8,35 @@ import { SpeakService } from '../servicios/speak.service';
 })
 export class ContactoComponent implements OnInit {
 
-  constructor(private speak:SpeakService) { }
+  constructor(private speak:SpeakService) {
+
+    this.start();
+    this.pause();
+    this.cancelar();
+    this.resumen();
+   }
 
   ngOnInit(): void {
+  }
+
+  start(){
+    this.speak.start();
+    console.log("f1");
+  }
+
+  pause(){
+    this.speak.pause();
+    console.log('f2');
+  }
+
+  cancelar(){
+    this.speak.cancelar();
+    console.log("f3");
+  }
+
+  resumen(){
+    this.speak.resume();
+    console.log("f4");
   }
 
 }
