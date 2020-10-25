b2bc57fe9618f5f84b08479170402f89609024fe
Paco-Medina <pacomedina.05.12.99@outlook.com>
2020-10-25 13:13:17 -0600

    speak tss en home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,42 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { SpeakService } from '../servicios/speak.service';
+import Speech from 'speak-tts';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit {  
+  datos:any;
+  speech:any;
 
-  constructor(private speak:SpeakService) { 
-
+  constructor() {
+    this.speech = new Speech();
+    if (this.speech.hasBrowserSupport()) {
+      // returns a boolean
+      console.log('speech synthesis supported');
+      this.speech.init({
+          volume: 1,
+          lang: 'es-MX',
+          rate: 1,
+          pitch: 1,
+          voice: 'Microsoft Sabina Desktop - Spanish (Mexico)',
+          splitSentences: true
+        });
+        
+    }
+    this.datos += "¿Quiénes Somos?..¿Qué es E-Tienda?.Somos la tienda online de abarrotes líder de América Latina";
     this.start();
-    this.pause();
-    this.cancelar();
-    this.resumen();
+   }
+
+   start(){
+    console.log(this.datos);
+    this.speech.speak({ 
+      text: this.datos,
+    });
   }
 
   ngOnInit(): void {
-  }
-
-  start(){
-    this.speak.start();
-    console.log("f1");
-  }
-
-  pause(){
-    this.speak.pause();
-    console.log('f2');
-  }
-
-  cancelar(){
-    this.speak.cancelar();
-    console.log("f3");
-  }
-
-  resumen(){
-    this.speak.resume();
-    console.log("f4");
   }
 
 }
